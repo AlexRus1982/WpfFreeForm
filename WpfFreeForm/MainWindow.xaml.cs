69e4b7a87db2a41906be99bfd780e932fffb71f6
Alexander Unknown <Alexanderwk@ya.ru>
2020-12-17 23:35:11 +0300

    Pattern structure added

---
+++ WpfFreeForm/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Media;
+using System.IO;
+
+namespace WpfFreeFormModulePlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const uint musicBufferLen_ms = 500;
+        const uint samplesPerSecond = 44100;
+        const uint musicBufferLen = (uint)((decimal)samplesPerSecond * musicBufferLen_ms / 1000);
+
+        private SoundPlayer player = new SoundPlayer();
+
+        private BinaryWriter writer0 = new BinaryWriter(new MemoryStream());
+        private BinaryWriter writer1 = new BinaryWriter(new MemoryStream());
+        private uint musicBuffer = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WriteWavHeader(writer0); GenerateWaveform(writer0, 0, true);
+            WriteWavHeader(writer1); GenerateWaveform(writer1, 0, true);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SoundButton_Click(object sender, RoutedEventArgs e)
+        {
+            Play(1500);
+        }
+
+        public void Play(uint frequency)
+        {
+            BinaryWriter writer = (musicBuffer == 0) ? writer0 : writer1;
+
+            player.Stream = writer.BaseStream;
+
+            DebugMes("Current buffer - " + musicBuffer);
+            DebugMes("Playing start ...");
+
+            player.PlaySync();
+
+            GenerateWaveform(writer, frequency);
+
+            DebugMes("Playing stop.");
+
+            musicBuffer = musicBuffer ^ 1; //SwapBuffers
+        }
+
+        private void DebugMes(string mes)
+        {
+            #if DEBUG
+            System.Diagnostics.Debug.WriteLine(mes);
+            #endif
+        }
+
+        private void WriteWavHeader(BinaryWriter writer)
+        {
+            char[] chunkId = { 'R', 'I', 'F', 'F' };
+            char[] format = { 'W', 'A', 'V', 'E' };
+            char[] subchunk1Id = { 'f', 'm', 't', ' ' };
+            char[] subchunk2Id = { 'd', 'a', 't', 'a' };
+            uint subchunk1Size = 16;
+            uint headerSize = 8;
+            ushort audioFormat = 1;
+            ushort numChannels = 1;  // Mono - 1, Stereo - 2
+            ushort bitsPerSample = 16;
+            ushort blockAlign = (ushort)(numChannels * ((bitsPerSample + 7) / 8));
+            uint sampleRate = samplesPerSecond;
+            uint byteRate = sampleRate * blockAlign;
+            uint waveSize = 4;
+            uint subchunk2Size = musicBufferLen * blockAlign;
+            uint chunkSize = waveSize + headerSize + subchunk1Size + headerSize + subchunk2Size;
+
+            writer.Write(chunkId);
+            writer.Write(chunkSize);
+            writer.Write(format);
+            writer.Write(subchunk1Id);
+            writer.Write(subchunk1Size);
+            writer.Write(audioFormat);
+            writer.Write(numChannels);
+            writer.Write(sampleRate);
+            writer.Write(byteRate);
+            writer.Write(blockAlign);
+            writer.Write(bitsPerSample);
+            writer.Write(subchunk2Id);
+            writer.Write(subchunk2Size);           
+        }
+
+        private void GenerateWaveform(BinaryWriter writer, uint frequency, bool zero = false)
+        {
+            const double TAU = 2 * Math.PI;
+            double theta = frequency * TAU / (double)samplesPerSecond;
+            // 'volume' is UInt16 with range 0 thru Uint16.MaxValue ( = 65 535)
+            // we need 'amp' to have the range of 0 thru Int16.MaxValue ( = 32 767)
+            uint volume = 16383;
+            double amp = volume >> 2; // so we simply set amp = volume / 2
+
+            writer.BaseStream.Seek(44, SeekOrigin.Begin);
+
+            for (int step = 0; step < musicBufferLen; step++)
+            {
+                short s = (short)(volume * Math.Sin(theta * (double)step));
+                writer.Write((zero) ? 0 : s);
+            }
+
+            writer.BaseStream.Seek(0, SeekOrigin.Begin);
+        }
+
+    }
+}
---
+++ WpfFreeForm/ModulePlayer/MODPlayer.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace WpfFreeFormModulePlayer.ModulePlayer
+{
+    public static class MODConst
+    {
+
+		public static string[] noteStrings = 
+		{
+		 	"C-", "C#", "D-", "D#", "E-", "F-", "F#", "G-", "G#", "A-", "A#", "B-"
+		};
+	
+		public static int[] ModSinusTable = 
+		{
+			   0,   24,   49,   74,   97,  120,  141,  161,  180,  197,  212,  224,  235,  244,  250,  253,
+			 255,  253,  250,  244,  235,  224,  212,  197,  180,  161,  141,  120,   97,   74,   49,   24,
+			   0,  -24,  -49,  -74,  -97, -120, -141, -161, -180, -197, -212, -224, -235, -244, -250, -253,
+			-255, -253, -250, -244, -235, -224, -212, -197, -180, -161, -141, -120,  -97,  -74,  -49,  -24
+		};
+
+		public static int[] ModRampDownTable =
+		{
+			   0,   -8,  -16,  -24,  -32,  -40,  -48,  -56,  -64,  -72,  -80,  -88,  -96, -104, -112, -120,
+			-128, -136, -144, -152, -160, -168, -176, -184, -192, -200, -208, -216, -224, -232, -240, -248,
+			 255,  247,  239,  231,  223,  215,  207,  199,  191,  183,  175,  167,  159,  151,  143,  135,
+			 127,  119,  113,  103,   95,   87,   79,   71,   63,   55,   47,   39,   31,   23,   15,    7
+		};
+
+		public static int[] ModSquareTable =
+		{
+			 255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,
+			 255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,  255,
+			-255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255,
+			-255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255, -255
+		};
+
+		public static int[] ModRandomTable =
+		{
+			 196, -254,  -86,  176,  204,   82, -130, -188,  250,   40, -142, -172, -140,  -64,  -32, -192,
+			  34,  144,  214,  -10,  232, -138, -124,  -80,   20, -122,  130,  218,  -36,  -76,  -26, -152,
+			 -46,  176,   42, -188,   16,  212,   42, -224,   12,  218,   40, -176,  -60,   18, -254,  236,
+			  84,  -68,  178,   -8, -102, -144,   42,  -58,  224,  246,  168, -202, -184,  196, -108, -190
+		};
+	
+		public static int[] ft2VibratoTable =
+		{
+			  0,  -2,  -3,  -5,  -6,  -8,  -9, -11, -12, -14, -16, -17, -19, -20, -22, -23,
+			-24, -26, -27, -29, -30, -32, -33, -34, -36, -37, -38, -39, -41, -42, -43, -44,
+			-45, -46, -47, -48, -49, -50, -51, -52, -53, -54, -55, -56, -56, -57, -58, -59,
+			-59, -60, -60, -61, -61, -62, -62, -62, -63, -63, -63, -64, -64, -64, -64, -64,
+			-64, -64, -64, -64, -64, -64, -63, -63, -63, -62, -62, -62, -61, -61, -60, -60,
+			-59, -59, -58, -57, -56, -56, -55, -54, -53, -52, -51, -50, -49, -48, -47, -46,
+			-45, -44, -43, -42, -41, -39, -38, -37, -36, -34, -33, -32, -30, -29, -27, -26,
+			-24, -23, -22, -20, -19, -17, -16, -14, -12, -11,  -9,  -8,  -6,  -5,  -3,  -2,
+			  0,   2,   3,   5,   6,   8,   9,  11,  12,  14,  16,  17,  19,  20,  22,  23,
+			 24,  26,  27,  29,  30,  32,  33,  34,  36,  37,  38,  39,  41,  42,  43,  44,
+			 45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  56,  57,  58,  59,
+			 59,  60,  60,  61,  61,  62,  62,  62,  63,  63,  63,  64,  64,  64,  64,  64,
+			 64,  64,  64,  64,  64,  64,  63,  63,  63,  62,  62,  62,  61,  61,  60,  60,
+			 59,  59,  58,  57,  56,  56,  55,  54,  53,  52,  51,  50,  49,  48,  47,  46,
+			 45,  44,  43,  42,  41,  39,  38,  37,  36,  34,  33,  32,  30,  29,  27,  26,
+			 24,  23,  22,  20,  19,  17,  16,  14,  12,  11,   9,   8,   6,   5,   3,   2
+		};
+
+		public const int NORM_MAX_PERIOD		= 907;
+		public const int NORM_MIN_PERIOD		= 108;
+		public const int EXT_MAX_PERIOD			= 1814;
+		public const int EXT_MIN_PERIOD			= 54;
+		public const int MAX_PERIOD				= EXT_MAX_PERIOD;
+		public const int MIN_PERIOD				= EXT_MIN_PERIOD;
+		public const int BASEFREQUENCYt			= 8363;
+		public const int BASEPERIOD				= 428;
+		public const int MAXVOLUME				= 64;
+		public const int SM_16BIT				= 0x04;	// 16 BIT
+		public const int SM_STEREO				= 0x08;	// STEREO
+		public const int LOOP_OFF				= 0x00;
+		public const int LOOP_ON				= 0x01;
+
+		public static string getNoteNameToIndex(int index)
+		{
+			if (index <  -1) return "---";
+			if (index ==  0) return "...";  // No Note
+			if (index == -1) return "***"; // Note cut value
+			return (noteStrings[(index - 1) % 12] + (int) (((index - 1) / 12) + 3));
+		}
+
+		public static int getNoteIndexForPeriod(int period)
+		{
+			if (period ==  0) return 0;		// No Note
+			if (period == -1) return -1;	// Note cut value
+			int note = 0;
+			for (int i = 1; i < 6; i++)
+			{
+				for (int j = 0; j < 12; j++)
+				{
+					int diff1 = getNotePeriod(note - 1, 0) - period;
+					int diff2 = period - getNotePeriod(note, 0);
+					if (diff1 < diff2) return note - 1;
+					note++;
+				}
+			}
+			return note;
+		}
+
+		public static int getNotePeriod(int note, int finetune)
+		{
+			int period = 7680 - (note + 24) * 64 - finetune * 8;
+			int frequency = (int)(8363 * Math.Pow(2, ((4608 - period) * 0.001302083)));
+			return (int)(3579545.25f / frequency);
+		}
+
+		public static int getNoteFreq(int note, int finetune)
+		{
+			int period = 7680 - (note + 24) * 64 - finetune * 8;
+			int frequency = (int)(8363 * Math.Pow(2, ((4608 - period) * 0.001302083)));
+			return frequency;
+		}
+		
+	}
+
+	public static class MODUtils
+	{
+		public static string VERSION 				= "V1.0";
+		public static string PROGRAM 				= "Sound engine";
+		public static string FULLVERSION 			= PROGRAM + " " + VERSION;
+		public static string COPYRIGHT 				= "Copyright by Alex 2006/07/08/09/10";
+	
+		public static string CODING_GUI 			= "cp850";
+		public static string CODING_COMMANLINE 		= "cp1252";
+		public static string currentCoding 			= CODING_GUI;
+
+		public const int SOUNDFREQUENCY 			= 44100;
+		public const int SOUNDBITS 					= 16;
+		public const int LOOP_ON					= 0x01;
+		public const int LOOP_SUSTAIN_ON			= 0x02;
+		public const int LOOP_IS_PINGPONG			= 0x04;
+		public const int LOOP_SUSTAIN_IS_PINGPONG	= 0x08;
+		
+		public static string getAsHex(int value, int digits)
+		{
+			string hex = value.ToString("X" + digits.ToString());
+			return hex;
+		}
+
+		public static string getAsDec(int value, int digits)
+		{
+			string dec = value.ToString("D" + digits.ToString());
+			return dec;
+		}
+
+	}
+
+
+	public class MODSample
+	{
+		public string name 				= "";	// Name of the sample
+		public int length				= 0;	// full length (already *2 --> Mod-Fomat)
+		public int fineTune				= 0;	// Finetuning -8..+8
+		public float volume				= 0;	// Basisvolume
+		public int repeatStart			= 0;	// # of the loop start (already *2 --> Mod-Fomat)
+		public int repeatStop			= 0;	// # of the loop end   (already *2 --> Mod-Fomat)
+		public int repeatLength			= 0;	// length of the loop
+		public int loopType				= 0;	// 0: no Looping, 1: normal, 2: pingpong, 3: backwards
+		public int baseFrequency		= 0;	// BaseFrequency
+	
+		public List<float> sampleData   = new List<float>();	// The sampledata, already converted to 16 bit (always)
+																// 8Bit: 0..127,128-255; 16Bit: -32768..0..+32767
+												
+		//private var snd:Sound 			= new Sound();
+		//private var channel:SoundChannel;
+		//private var playPos:Number		= 0;
+		//private var playInc:Number		= 1;
+		//private var loopStart:Boolean   = false;
+
+
+		public MODSample()
+		{
+			//super();
+			//snd.addEventListener(SampleDataEvent.SAMPLE_DATA, mixSound);
+		}
+
+		//public function playSound():void
+		//{
+		//	playPos = 0;
+		//	playInc = Number(this.baseFrequency / 44100);
+		//loopStart = false;
+		//	channel = snd.play();
+		//}
+
+		//private void mixSound(event:SampleDataEvent)
+		//{
+		//	for (var i:int = 0; i< 4096; i++)
+		//	{
+		//		var n:Number = 0;
+		//		if (int(playPos) <= this.length)
+		//		{
+		//			n = this.sampleData[int(playPos)];
+		//			event.data.writeFloat(n);
+		//			event.data.writeFloat(n);
+		//		}
+				
+		//		if ((playPos >= length) && (!loopStart) && (loopType == MODConst.LOOP_ON))
+		//			loopStart = true;
+					
+		//		if ((playPos >= repeatStop) && (loopStart))
+		//			playPos = repeatStart;
+
+		//		playPos += playInc;
+		//	}
+		//}
+
+		public void fixSampleLoops()
+		{
+			if (sampleData == null || length == 0)
+			{
+				repeatStop = repeatStart = 0;
+				repeatLength = repeatStop - repeatStart;
+				loopType = 0;
+				return;
+			}
+			if (repeatStop > length)
+			{
+				repeatStop = length;
+				repeatLength = repeatStop - repeatStart;
+			}
+			if (repeatStart + 2 > repeatStop)
+			{
+				repeatStop = repeatStart = 0;
+				repeatLength = repeatStop - repeatStart;
+				loopType = 0;
+			}
+		}
+
+		public override string ToString()
+		{
+			///*
+			//if (length == 0) return this.name;
+			string res = this.name;
+			res += ('(')
+				   + "len:" + length + ","
+				   + "fTune:" + fineTune + ","
+				//+ "transpose:"  + transpose + ","
+				+ "baseFreq:" + baseFrequency + ","
+				   + "vol:" + volume + ","
+				//+ "panning:" + panning + ","
+				+ "repStart:" + repeatStart + ","
+				   + "repLen:" + repeatLength + ","
+				   + "repStop:" + repeatStop + ")";
+
+			return res;
+			//*/
+			//return this.toShortString();
+		}
+
+		public string toShortString()
+		{
+			return this.name;
+		}
+
+		//public void readSampleHeader(fileData:ByteArray)
+		//{
+		//	name = fileData.readMultiByte(22, 'cp866'); // Samplename
+		//	length = fileData.readUnsignedShort() << 1; // Length
+
+		//	var fine:int = fileData.readUnsignedByte() & 0xF; // finetune Value>7 means negative 8..15= -8..-1
+		//	fine = (fine > 7) ? fine - 16 : fine;
+
+		//	fineTune = fine;
+		//	baseFrequency = cMODConst.getNoteFreq(24, fine);
+
+		//	var vol:int = fileData.readUnsignedByte() & 0x7F; // volume 64 is maximum
+		//	volume = (vol > 64) ? 1.0 : Number(vol / 0x40);
+
+		//	// Repeat start and stop
+		//	repeatStart = fileData.readUnsignedShort() << 1;
+		//	repeatLength = fileData.readUnsignedShort() << 1;
+		//	repeatStop = repeatStart + repeatLength;
+
+		//	if (length < 4) length = 0;
+		//	if (length > 0)
+		//	{
+		//		if (repeatStart > length) repeatStart = length;
+		//		if (repeatStop > length) repeatStop = length;
+		//		if (repeatStart >= repeatStop ||
+		//			repeatStop <= 8 ||
+		//			(repeatStop - repeatStart) <= 4)
+		//		{
+		//			repeatStart = repeatStop = 0;
+		//			loopType = 0;
+		//		}
+		//		if (repeatStart < repeatStop) loopType = cMODConst.LOOP_ON;
+		//	}
+		//	else loopType = 0;
+		//	repeatLength = repeatStop - repeatStart;
+		//}
+
+		public void readSampleData(BinaryReader reader)
+		{
+			if (length > 0)
+			{
+				sampleData = new List<float>();
+				for (int s = 0; s < length; s++)
+				{
+					int data = reader.ReadByte();
+					sampleData.Add((float)((data - 127) * 0.0078125));  // 0.0078125 = 1/128
+				}
+			}
+			fixSampleLoops();
+		}		
+		
+	}
+
+	public class MODInstrument
+	{
+		//public array sampleIndex			= [];
+		//public var noteIndex:Array				= [];
+		public int volumeFadeOut			= 0;
+		public string name					= "Noname instrument";
+	
+		// Impulstracker Values:
+		public string dosFileName			= "";
+		public int dublicateNoteCheck		= 0;
+		public int dublicateNoteAction		= 0;
+		/**
+	 	* NNA: New note action:
+		*      0 = Note cut
+		*      1 = Note continue
+		*      2 = Note off
+		*      3 = Note fade
+		*/
+		public int NNA						= 0;
+		public int pitchPanSeparation		= 0;
+		public int pitchPanCenter			= 0;
+		public int globalVolume				= 0;
+		public int defaultPan				= 0;
+		public int randomVolumeVariation 	= 0;
+		public int randomPanningVariation	= 0;
+		public int initialFilterCutoff		= 0;
+		public int initialFilterResonance	= 0;
+
+		public override string ToString()
+		{
+			return name;
+		}
+
+		//public function readInstrument(fileData:ByteArray):void
+		//{
+		//}
+	}
+
+	public class MODInstrumentsList
+	{
+		//public var sample:Array = [];
+
+		public int getAllSamplesLength()
+		{
+			int allSamplesLength = 0;
+			//for (int i = 0; i < sample.length; i++)
+			//	allSamplesLength += sample[i].length;
+			return allSamplesLength;
+		}
+
+		public override string ToString()
+		{
+			string result = "";
+
+			//if (sample.length > 0)
+			//{
+			//	result += 'Samples info : \n';
+			//	for (var i:int = 0; i < sample.length; i++)
+			//			{
+			//		result += (MODUtils.getAsHex(i, 2) + ':' + sample[i].toString());
+			//		result += '\n';
+			//	}
+			//}
+
+			return result;
+		}
+	}
+
+}
